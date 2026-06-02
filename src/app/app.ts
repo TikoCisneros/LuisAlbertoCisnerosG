b67ca0b77c13cbbf1ccf285f27314ba5784d6fb2
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2026-06-02 17:14:23 -0500

    feat: aplicar el layout en AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MainLayoutComponent } from './shared/layouts/main-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainLayoutComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('LuisAlbertoCisnerosG');
-}
+export class App {}
